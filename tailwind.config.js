374beff3d870db083efee838c1eb3f85c5565dd2
Ian <ianluong@hotmail.co.uk>
2022-07-23 18:04:17 +0100

    Setup TailwindCSS functionality

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        fontFamily: {
+            'inter': ["'Inter'", 'sans-serif'],
+            'roboto': ["'Roboto'", 'sans-serif'],
+            'ibarra': ["'Ibarra Real Nova'", 'serif'],
+        },
+        extend: {},
+        plugins: [],
+    }
+}
