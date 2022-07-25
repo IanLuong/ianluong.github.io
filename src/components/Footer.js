016de536aa8bb1db69b796542f357da04ebba3da
Ian <ianluongcodes@gmail.com>
2022-07-25 14:02:53 +0100

    Add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,39 @@
-import email from "../img/email.svg"
-import instagram from "../img/instagram.svg"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faLinkedin,
+  faInstagram,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
-    return (
-        <footer class="font-roboto flex text-white bg-blue-400 flex-col p-1 pb-12">
-        <h1 class="font-ibarra font-semibold self-center text-5xl">Contact me</h1>
-        <p class="subtitle">Please get in touch if you think my work could be beneficial!</p>
+  return (
+    <footer class="font-roboto flex bg-blue-400 flex-col p-6 gap-4">
+      <h1 class="font-ibarra text-white font-semibold self-center text-6xl">
+        Contact me
+      </h1>
+      <p class="text-white font-bold">
+        Please get in touch if you think my work could be beneficial!
+      </p>
+      <div>
+        <p class="text-white">1234 Random Road</p>
+        <p class="text-white">Random Town, Calfornia 12345</p>
+      </div>
+      <div>
+        <p class="text-white">555-555-5555</p>
+        <p class="text-white">ianluongcodes@gmail.com</p>
+      </div>
 
-        <div>
-            <div class="flex gap-2 items-center">
-                <img class="w-16 h-12" src={email} alt="" />
-                <img class="w-16 h-12" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="" />
-                <img class="w-16 h-12" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt="" />
-                <img class="w-16 h-12" src={instagram} alt="" />
-            </div>
-        </div>
-
+      <div class="flex gap-4 items-center self-center">
+        <FontAwesomeIcon
+          icon={faLinkedin}
+          size="3x"
+          className="hover:text-white"
+        />
+        <FontAwesomeIcon icon={faInstagram} size="3x" />
+        <FontAwesomeIcon icon={faGithub} size="3x" />
+        <FontAwesomeIcon icon={faEnvelope} size="3x" />
+      </div>
     </footer>
-
-    )
+  );
 }
---
+++ src/components/Project.js
@@ -0,0 +1,5 @@
+export default function Project({title, desc, imgPath}) {
+    return (
+        <h1>Project</h1>
+    )
+}
