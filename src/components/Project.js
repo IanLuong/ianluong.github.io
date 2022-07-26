d7d3a39a391221744641eb29d6b21e2d62b1b36f
Ian <ianluongcodes@gmail.com>
2022-07-26 12:48:07 +0100

    Add projects section

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,24 @@
-export default function Project({title, desc, imgPath}) {
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+export default function Project(props) {
+    const {title, desc, imgPath} = props 
     return (
-        <h1>Project</h1>
+        <div className="w-full shadow-xl">
+            {imgPath ? <img className="w-full h-48" src={imgPath} alt=""/> : <div className="w-full h-48 bg-red-700"></div>}
+            
+            <div className="px-4 pt-2 pb-8">
+                <div className="flex items-center">
+                    <h2 className="font-ibarra text-lg mr-auto">{title}</h2>
+                    <div className="flex gap-4">
+                        <FontAwesomeIcon icon={faGithub} size='lg'/>
+                        <FontAwesomeIcon icon={faUpRightFromSquare} size='lg'/>
+                    </div>
+                </div>
+                <p className="font-inter">{desc}</p>
+            </div>
+        </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,20 @@
+import Project from "./Project";
+
 export default function Projects() {
 
 return (
-    <section>
-      <div className="flex flex-col p-6 gap-4">
-        <h1 className="font-ibarra font-semibold self-center text-6xl">
-          About me
-        </h1>
+  <section>
+    <div className="flex flex-col px-6 py-8 gap-4">
+      <h1 className="font-ibarra font-semibold self-center text-6xl">
+        My work
+      </h1>
+
+      <div className="flex flex-col gap-8">
+        <Project title="Spoofy Records" desc="Record store website produced to replicate an online shopping experience. Built with React." />
+        <Project title="Anime Quiz" desc="Mini quiz app made to entertain a growing, local anime community. Built using React and questions supplied by the Open Trivia Database API." />
+        <Project title="Kamatimer" desc="Pomodoro Timer Chrome Extension created to keep focused. Built using React and Typescript." />
       </div>
-    </section>
-  );
+    </div>
+  </section>
+);
 }
