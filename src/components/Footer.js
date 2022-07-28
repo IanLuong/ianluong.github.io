5842b2fbc0e48b6619ad428a526447e1fd65c68b
Ian <ianluongcodes@gmail.com>
2022-07-28 01:53:51 +0100

    Add large screen responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,8 @@ import footerPortrait from "../img/pexels-enes-bayraktar-11306305.jpg"
 
 export default function Footer() {
   return (
-    <footer className="md:flex md:justify-around bg-blue-400">
-      <div className="md:flex md:w-2/3">
+    <footer className="md:flex md:justify-center bg-blue-400">
+      <div className="md:flex md:w-2/3 justify-center">
         <div class="font-inter flex flex-col p-6 gap-4 md:justify-center">
           <h1 class="font-ibarra text-white font-semibold self-center text-6xl md:self-start">
             Contact me
--- src/components/Project.js
+++ src/components/Project.js
@@ -6,16 +6,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Project(props) {
     const {title, desc, imgPath, repoPath, sourcePath} = props 
     return (
-      <div className="w-full max-w-lg max-h-80 shadow-xl justify-self-center">
+      <div className="w-full max-w-sm max-h-64 shadow-xl justify-self-center">
         {imgPath ? (
-          <img className="w-full h-48" src={imgPath} alt="" />
+          <img className="w-full h-32" src={imgPath} alt="" />
         ) : (
-          <div className="w-full h-48 bg-red-700"></div>
+          <div className="w-full h-32 bg-red-700"></div>
         )}
 
-        <div className="px-4 pt-2 pb-16">
-          <div className="flex items-center">
-            <h2 className="font-ibarra text-lg mr-auto">{title}</h2>
+        <div className="px-4 pt-2 p-16">
+          <div className=" flex items-center">
+            <h2 className="font-ibarra text-xl mr-auto">{title}</h2>
             <div className="flex gap-4">
               {repoPath && (
                 <a href={repoPath}>
