cd00c1bd04ac5e0a9a1b1b616f52811b601e523f
Ian <ianluong@hotmail.co.uk>
2022-08-03 00:07:12 +0100

    Adjust animations

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,66 +1,66 @@
-import portrait from "../img/pexels-kassandre-pedro-12798416.jpg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowCircleDown } from "@fortawesome/free-solid-svg-icons";
-import { useRef } from "react";
-
+import portrait from "../img/pexels-kassandre-pedro-12798416.jpg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowCircleDown } from "@fortawesome/free-solid-svg-icons"
+import { useRef } from "react"
 
 export default function Hero() {
-    const scrollRef = useRef()
-  
-    return (
-      <section className="bg-red-400">
-        <div className="bg-red-400 lg:m-auto lg:max-w-screen-lg">
-          <div className="relative flex flex-col md:flex-row bg-red-400 min-h-screen justify-center items-center md:m-auto md:w-2/3">
-            <img className="w-3/4 md:w-11/12" src={portrait} alt="" />
-            <h1 className="font-ibarra drop-shadow-md absolute text-6xl text-white right-8 top-32 text-right animate-fade delay-2000">
-              Frontend <br /> Developer
+  const scrollRef = useRef()
+
+  return (
+    <section className="bg-red-400">
+      <div className="bg-red-400 lg:m-auto lg:max-w-screen-lg">
+        <div className="relative flex flex-col md:flex-row bg-red-400 min-h-screen justify-center items-center md:m-auto md:w-2/3">
+          <img
+            className="w-3/4 md:w-11/12 animate-fade2s"
+            src={portrait}
+            alt=""
+          />
+          <h1 className="font-ibarra drop-shadow-md absolute text-6xl text-white right-8 top-32 text-right animate-fade4s delay-2000">
+            Frontend <br /> Developer
+          </h1>
+          <h1 className="font-ibarra drop-shadow-md absolute text-6xl text-white left-8 bottom-32 animate-fade6s">
+            Ian <br /> Luong
+          </h1>
+          <FontAwesomeIcon
+            className="animate-bounce text-white hover:text-black cursor-pointer drop-shadow-md absolute bottom-4"
+            icon={faArrowCircleDown}
+            size="3x"
+            onClick={() =>
+              scrollRef.current.scrollIntoView({ behavior: "smooth" })
+            }
+          />
+        </div>
+      </div>
+
+      <div className="bg-white">
+        <div className="md:m-auto md:w-2/3 lg:w-1/2" ref={scrollRef}>
+          <div className="flex flex-col p-6 gap-4 font-inter">
+            <h1 className="font-ibarra font-semibold self-center text-6xl">
+              About me
             </h1>
-            <h1 className="font-ibarra drop-shadow-md absolute text-6xl text-white left-8 bottom-32 animate-fade">
-              Ian <br /> Luong
-            </h1>
-            <FontAwesomeIcon
-              className="animate-bounce text-white hover:text-black cursor-pointer drop-shadow-md absolute bottom-4"
-              icon={faArrowCircleDown}
-              size="3x"
-              onClick={() =>
-                scrollRef.current.scrollIntoView({ behavior: "smooth" })
-              }
-            />
+
+            <p>
+              Rerum molestias et nulla soluta iste a autem deserunt. Libero non
+              non et rerum sunt ea non. Repudiandae possimus ipsa sequi. Aut
+              praesentium facilis quidem quo qui quasi non dolores vel. Beatae
+              voluptates et reprehenderit dolores dolore.
+            </p>
+            <p>
+              Totam placeat id placeat quod quaerat dolores molestiae aut
+              ducimus. Dolor qui exercitationem expedita omnis vel iure
+              doloribus. Dolor laboriosam aut qui reiciendis voluptas maxime
+              voluptatibus maxime sed.
+            </p>
+            <p>
+              Odio sed sit dolores et eligendi repellat magni. Quibusdam eos
+              facere. Eius debitis voluptas aut velit consequatur ratione rerum
+              sed. Ab alias doloremque rerum. Minus nihil commodi voluptatem
+              amet blanditiis aut laboriosam eum et. Consequatur possimus omnis
+              libero est quia dolor.
+            </p>
           </div>
         </div>
-
-        <div className="bg-white">
-          <div
-            className="md:m-auto md:w-2/3 lg:w-1/2"
-            ref={scrollRef}
-          >
-            <div className="flex flex-col p-6 gap-4 font-inter">
-              <h1 className="font-ibarra font-semibold self-center text-6xl">
-                About me
-              </h1>
-
-              <p>
-                Rerum molestias et nulla soluta iste a autem deserunt. Libero
-                non non et rerum sunt ea non. Repudiandae possimus ipsa sequi.
-                Aut praesentium facilis quidem quo qui quasi non dolores vel.
-                Beatae voluptates et reprehenderit dolores dolore.
-              </p>
-              <p>
-                Totam placeat id placeat quod quaerat dolores molestiae aut
-                ducimus. Dolor qui exercitationem expedita omnis vel iure
-                doloribus. Dolor laboriosam aut qui reiciendis voluptas maxime
-                voluptatibus maxime sed.
-              </p>
-              <p>
-                Odio sed sit dolores et eligendi repellat magni. Quibusdam eos
-                facere. Eius debitis voluptas aut velit consequatur ratione
-                rerum sed. Ab alias doloremque rerum. Minus nihil commodi
-                voluptatem amet blanditiis aut laboriosam eum et. Consequatur
-                possimus omnis libero est quia dolor.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
-    );
+      </div>
+    </section>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: 'jit',
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-        fontFamily: {
-            'inter': ["'Inter'", 'sans-serif'],
-            'ibarra': ["'Ibarra Real Nova'", 'serif'],
-        },
-        extend: {
-            animation: {
-                fade: 'fade 2s ease-in-out',
-            },
-            keyframes: {
-                fade: {
-                    '0%' : { opacity: 0 },
-                    '100%': { opacity: 1 }
-                },
-            }
-        },
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      inter: ["'Inter'", "sans-serif"],
+      ibarra: ["'Ibarra Real Nova'", "serif"],
     },
-    plugins: [],
+    extend: {
+      animation: {
+        fade2s: "fade 2s ease-in-out",
+        fade4s: "fade 4s ease-in-out",
+        fade6s: "fade 4s ease-in-out",
+      },
+      keyframes: {
+        fade: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [],
 }
