f509899a56344e3ecf4aa8a0b0a7115584840710
Ian <ianluong@hotmail.co.uk>
2022-08-02 20:04:40 +0100

    Revert "Updates"
    This reverts commit ddea1390c2ecac5e55ca273eb6584c4c92446003.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import Projects from "./components/Projects";
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/Project.js
@@ -0,0 +1,36 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+export default function Project(props) {
+    const {title, desc, imgPath, repoPath, sourcePath} = props 
+    return (
+      <div className="w-full max-w-sm max-h-64 shadow-xl justify-self-center">
+        {imgPath ? (
+          <img className="w-full h-32" src={imgPath} alt="" />
+        ) : (
+          <div className="w-full h-32 bg-red-700"></div>
+        )}
+
+        <div className="px-4 pt-2 p-16">
+          <div className=" flex items-center">
+            <h2 className="font-ibarra text-xl mr-auto">{title}</h2>
+            <div className="flex gap-4">
+              {repoPath && (
+                <a href={repoPath} className="hover:opacity-30 transition-opacity ease-in delay-75">
+                  <FontAwesomeIcon icon={faGithub} size="xl" />
+                </a>
+              )}
+              {sourcePath && (
+                <a href={sourcePath} className="hover:opacity-30 transition-opacity ease-in delay-75">
+                  <FontAwesomeIcon icon={faUpRightFromSquare} size="xl" />
+                </a>
+              )}
+            </div>
+          </div>
+          <p className="font-inter italic">{desc}</p>
+        </div>
+      </div>
+    );
+}
