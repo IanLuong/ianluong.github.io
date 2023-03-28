3738f15decb8501e8ecc71d24dfe72e411f4a8a0
Ian <ianluongcodes@gmail.com>
2023-03-28 16:37:46 +0100

    Update whatdoio project listing

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,9 @@
-import Project from "./Project"
-import kamatimer from "../img/kamatimer.png"
-import quiz from "../img/quiz.png"
-import spoofy from "../img/spoofy.png"
-import portfolio from "../img/portfolio.png"
-import whatdoio from "../img/whatdoio.png"
+import Project from './Project'
+import kamatimer from '../img/kamatimer.png'
+import quiz from '../img/quiz.png'
+import spoofy from '../img/spoofy.png'
+import portfolio from '../img/portfolio.png'
+import whatdoio from '../img/whatdoio.png'
 
 export default function Projects() {
   return (
@@ -17,9 +17,9 @@ export default function Projects() {
           <Project
             title="Spoofy Records"
             desc={[
-              "Record store website produced to replicate an online shopping experience. Features include a shopping cart, info pages for the records and more!",
-              "\n\n",
-              "(Feel free to take a peek at my music taste and maybe some records that I own)",
+              'Record store website produced to replicate an online shopping experience. Features include a shopping cart, info pages for the records and more!',
+              '\n\n',
+              '(Feel free to take a peek at my music taste and maybe some records that I own)',
             ]}
             imgPath={spoofy}
             builtWith="React and React Router"
@@ -30,21 +30,21 @@ export default function Projects() {
           <Project
             title="WhatDo.io"
             desc={[
-              "A Full Stack CRUD app for tracking shared expenses, so people can get paid back. Features user authentication to track your expenses on the go.",
-              "\n\n",
-              "Built the backend REST API using MongoDB, Express and Node.js, with the front end using React.",
+              'A Full Stack CRUD app for tracking shared expenses, so people can get paid back. Features user authentication to track your expenses on the go.',
+              '\n\n',
+              'Built the backend REST API using MongoDB, Express and Node.js, with the front end using React and Tailwind.',
             ]}
             imgPath={whatdoio}
             builtWith="MERN Stack"
-            repoPath="https://github.com/IanLuong/what-do-i-owe"
+            sourcePath="https://what-do-io.fly.dev/"
             isLeft={false}
           />
           <Project
             title="Anime Quiz"
             desc={[
-              "Mini quiz app made to entertain a growing, local anime community. Quiz questions are pulled from the Open Trivia DB API and contain both multiple choice and true or false questions.",
-              "\n\n",
-              "Features copy to clipboard score sharing so you can let the group chat lads know, you are the one and only GOAT.",
+              'Mini quiz app made to entertain a growing, local anime community. Quiz questions are pulled from the Open Trivia DB API and contain both multiple choice and true or false questions.',
+              '\n\n',
+              'Features copy to clipboard score sharing so you can let the group chat lads know, you are the one and only GOAT.',
             ]}
             imgPath={quiz}
             builtWith="React, Open Trivia Database API"
@@ -56,7 +56,7 @@ export default function Projects() {
             title="My Portfolio"
             desc={[
               "My portfolio showing off my work and some of my interests. Oh hey wait a second, you already knew that because you're here right now. ",
-              "\n\n",
+              '\n\n',
               "Huh, I guess you can click the link if you want. I'm pretty sure I know where it'll take you though.",
             ]}
             imgPath={portfolio}
@@ -67,8 +67,8 @@ export default function Projects() {
           <Project
             title="KamaTimer"
             desc={[
-              "A Pomodoro Timer Web App created for friends and family to keep focused, featuring customizable focus/break timing and a dark mode. The use-sound hook makes use of bell and tick sounds to announce breaks, ensuring you pace your learning and ace your goals!",
-              "\n\n",
+              'A Pomodoro Timer Web App created for friends and family to keep focused, featuring customizable focus/break timing and a dark mode. The use-sound hook makes use of bell and tick sounds to announce breaks, ensuring you pace your learning and ace your goals!',
+              '\n\n',
               "P.S. Named after 'kamatis' which means 'tomato' in Tagalog.",
             ]}
             imgPath={kamatimer}
