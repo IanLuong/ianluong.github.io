e5565a6342856a96622b3267af79a0ea4ec3445a
Ian <ianluong@hotmail.co.uk>
2023-03-28 23:08:12 +0100

    Add redesign vite skeleton

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <div>
+      <header className="min-h-screen flex flex-col justify-between">
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>My Work</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  mode: "jit",
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    backgroundImage: {
-      'bay-photo': "url('img/bay_photo.jpg')",
-    },
-    fontFamily: {
-      inter: ["'Inter'", "sans-serif"],
-      ibarra: ["'Ibarra Real Nova'", "serif"],
-    },
-    extend: {
-      colors: {
-        'pink': '#FA9999',
-        'dark-blue': '#002742',
-        'light-blue': '#B8E1FF'
-      },
-      animation: {
-        fade1s: "fade ease-in 1s",
-        fade3s: "fade ease-in 3s",
-        fade6s: "fade ease-in 6s",
-      },
-      keyframes: {
-        fade: {
-          "0%": { opacity: 0 },
-          "40%": { opacity: 0 },
-          "100%": { opacity: 1 },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
